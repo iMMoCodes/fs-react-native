66140000c9969bb1ab7466cbceb7902e191e1429
iMMoCodes <matti-makela@hotmail.com>
2022-03-08 02:41:34 +0200

    Tehtävä 10.4

---
+++ src/components/AppBarTab.jsx
@@ -0,0 +1,22 @@
+import { Pressable, StyleSheet } from 'react-native';
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  title: {
+    color: theme.colors.textWhite,
+    marginLeft: 10,
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes.subheading,
+  },
+});
+
+const AppBarTab = ({ title }) => {
+  return (
+    <Pressable>
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default AppBarTab;
